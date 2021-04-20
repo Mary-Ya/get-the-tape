5c3aae1223cfdd8db0d3b68a69ba25bd0ba6b4ac
Mary Y <notamary@bk.ru>
2021-04-20 12:18:33 +0200

    faet(app)

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react"
+import ReactDom from "react-dom"
+import App from "./app.tsx"
+import "./App.css"
+
+ReactDom.render(<App />, document.getElementById('app'))
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
+module.exports = {
+  // Where files should be sent once they are bundled
+ output: {
+   path: path.join(__dirname, '/..', '/public'),
+   filename: 'index.bundle.js'
+ },
+ entry: './src/index.js',
+ devtool: 'eval-cheap-module-source-map',
+  // Rules of how webpack will take our files, compile & bundle them for the browser 
+ module: {
+   rules: [
+    {
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              "@babel/preset-env",
+              "@babel/preset-react",
+              "@babel/preset-typescript",
+            ],
+          },
+        },
+      },
+     {
+       test: /\.css$/,
+       exclude: /node_modules/,
+       use: ['style-loader', 'css-loader']
+     }
+   ]
+ },
+ resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+ plugins: [
+   new HtmlWebpackPlugin({ template: './src/index.html' })
+ ]
+}
