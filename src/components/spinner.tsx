69d7a40dec70fc9536b0e3783cffd073304d7ea7
Mary Y <notamary@bk.ru>
2021-08-16 00:25:51 +0200

    ref(home): Spinner

---
+++ src/components/spinner.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function Spinner() {
+    return <div className="spinner-grow" role="status">
+    <span className="sr-only">Loading...</span>
+  </div>
+}
+
+
+export default Spinner;
