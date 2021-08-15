9f33246b19e732602654bac09a1f25e7bac3d60b
Mary Y <notamary@bk.ru>
2021-08-16 00:15:13 +0200

    style(app): nav

--- src/app.tsx
+++ src/app.tsx
@@ -46,7 +46,7 @@ const App = () => {
   return (
     <div>
       <div className="d-flex align-items-start">
-      <ul className="nav flex-column mt-lg-5 w-10 d-flex py-3 px-5">
+      <ul className="nav flex-column mt-lg-5 w-10 d-flex py-3 ps-3 pe-5">
           <li className="nav-item text-center">
           <svg xmlns="http://www.w3.org/2000/svg" width="47" height="47" fill="currentColor" className="bi bi-star" viewBox="0 0 16 16">
             <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
