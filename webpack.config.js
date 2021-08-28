7822d8056dd58545df3bf2ea48447fb26a22de19
Mary Y <notamary@bk.ru>
2021-08-28 15:39:02 +0200

    prev

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 // const MiniCssExtractPlugin  = require('mini-css-extract-plugin');
 module.exports = {
