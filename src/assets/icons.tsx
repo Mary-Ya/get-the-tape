a2e0e834e565cef3f9360de6304f119a3ee1b083
Mary Y <notamary@bk.ru>
2021-09-09 22:26:23 +0200

    fix(Icons): add Star

--- src/assets/icons.tsx
+++ src/assets/icons.tsx
@@ -59,4 +59,11 @@ const Inside = (props: any) => (<svg
   <path d="M2 1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H9.5a1 1 0 0 0-1 1v7.293l2.646-2.647a.5.5 0 0 1 .708.708l-3.5 3.5a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L7.5 9.293V2a2 2 0 0 1 2-2H14a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h2.5a.5.5 0 0 1 0 1H2z"></path>
 </svg>)
 
-export default { Pause, Play, Logo, Pencil, Save, Plus, Inside };
+const Star = (props: any) => (<svg xmlns="http://www.w3.org/2000/svg"
+  width={props.width || '47'}
+  height={props.height || '47'}
+  fill="currentColor" className="bi bi-star" viewBox="0 0 16 16">
+<path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
+</svg>)
+
+export default { Pause, Play, Logo, Pencil, Save, Plus, Inside, Star };
